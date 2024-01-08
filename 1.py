2d2035414885fd885329cbe7e05a51e9281d3d80
nguyenducthanh10032005 <ndthanh.dhkl17a2hn@sv.uneti.edu.vn>
2024-01-09 00:53:10 +0700

    first commit

---
+++ 1.py
@@ -0,0 +1,65 @@
+#Xét kết quả học tập
+diem_TB = eval(input("nhập điểm trung bình: "))
+if diem_TB >=5:
+    print
+
+
+
+
+
+
+
+    #Xét kết quả học tập if ...elif...else
+    diem_TB = eval((input("Nhập điểm trung bình: ")))
+    if diem_TB >=0 and diem_TB <=10:
+    if diem_TB < 5:
+        print("Yếu Kém!!!")
+    elif diem_TB <6:
+        print("trung bình - khá!")
+    elif diem_TB < 8:
+        print("khá!!!")
+    elif diem_TB < 9:
+        print("Giỏi!!!")
+    else:
+        print("Điểm nhập vào khoong hợp lệ!")
+
+
+
+
+
+        #Vòng_lặp_while
+        i=1
+        while i<=10:
+        print('i=',i)
+        i+=1
+              
+              
+
+
+
+
+
+print("Chương trình in bảng cửu chương 3")
+i=1
+while i<=10:
+    print('3x',i,'=',3*i)
+    i+=1
+
+
+
+
+
+
+
+
+
+
+
+    var = 1-0
+    while var > 8 :
+        if var %2 == 0:
+            print("Giá trị",var)
+            var -=1
+            if var ==5:
+                break
+            print("kết thúc vòng lặp!")
---
+++ bài_1.2.py
@@ -0,0 +1,7 @@
+x=10
+y=5
+print("x=10","y=5")
+print("Tổng: x+y =",x+y)
+print("Hiệu: x-y =",x-y)
+print("Tích: x*y =",x*y)
+print("Thương: x/y =",x)
